5fa3b85a55309d93a14c2ed61a1c4a070476346c
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-26 09:34:52 +0530

    chore: Wrap App with PersistGate, Redux store, and error boundary in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App.jsx';
+import { store, persistor } from './store/index.js';
+import { ErrorBoundary } from './components/componentsIndex.js';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <ErrorBoundary>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </Provider>
+        </ErrorBoundary>
     </StrictMode>
 );
