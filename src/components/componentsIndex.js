22247a30a5ad849bfcae3667f99411b0febe67cb
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-28 06:35:57 +0530

    update: Export Login, Open and Close eye icons

--- src/components/componentsIndex.js
+++ src/components/componentsIndex.js
@@ -2,3 +2,6 @@ export { default as ErrorBoundary } from './error-boundary/ErrorBoundary.jsx';
 export { default as ErrorBoundaryInRoutes } from './error-boundary/ErrorBoundaryInRoutes.jsx';
 export { default as ProtectedRoute } from './ProtectedRoute.jsx';
 export { default as Layout } from './layout/Layout.jsx';
+export { default as Login } from './login/Login.jsx';
+export { default as CloseEye } from './show-passowrd-eye-icons/CloseEye.jsx';
+export { default as OpenEye } from './show-passowrd-eye-icons/OpenEye.jsx';
