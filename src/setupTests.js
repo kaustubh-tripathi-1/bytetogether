a35be99c58cb09a37b485281d37c2a4d72f07e96
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-06-08 20:25:34 +0530

    feat(test): Mock sessionStorage web api

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom';
+
+// Mock sessionStorage
+const mockSessionStorage = (() => {
+    let store = {};
+    return {
+        getItem: (key) => store[key] || null,
+        setItem: (key, value) => (store[key] = value.toString()),
+        removeItem: (key) => delete store[key],
+        clear: () => (store = {}),
+    };
+})();
+
+Object.defineProperty(window, 'sessionStorage', {
+    value: mockSessionStorage,
+    writable: true,
+});
