3079333a6f9bac5e85aff7dae04153fccfee62db
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-06-12 18:57:39 +0530

    feat(data-fetching): Add Tanstack Query Client and Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,23 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 import App from './App.jsx';
 import { store, persistor } from './store/index.js';
 import { ErrorBoundary } from './components/componentsIndex.js';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <ErrorBoundary>
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
-                    <App />
+                    <QueryClientProvider client={queryClient}>
+                        <App />
+                    </QueryClientProvider>
                 </PersistGate>
             </Provider>
         </ErrorBoundary>
