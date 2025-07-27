4c24518bf5f625c3bd8f2caff1c3c99e19c26bde
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-07-28 05:10:10 +0530

    feat: add a util fn to get j0 lang id

---
+++ client/src/utils/getJudge0LanguageId.js
@@ -0,0 +1,10 @@
+import { judge0LanguagesIds } from '../conf/languages';
+
+/**
+ * Retrieves Judge0 language id for given language extension.
+ * @param {string} languageExtension - The programming language extension.
+ * @returns {number} Judge0 language id or id of plain text if undefined.
+ */
+export function getJudge0LanguageId(languageExtension) {
+    return judge0LanguagesIds[languageExtension] || 43;
+}
