96f8e9ed4bd4821eda6486e88bc214c917bc0abc
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-07-28 05:01:27 +0530

    feat: add proxy to backend to make api requests in dev

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,22 @@ import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss(), svgr()],
+export default defineConfig(({ mode }) => {
+    const isProduction = mode === 'production';
+
+    return {
+        plugins: [react(), tailwindcss(), svgr()],
+        //TODO remove proxy after deploy if using vercel functions
+        server: {
+            proxy: {
+                '/api': {
+                    target: isProduction
+                        ? 'https://bytetogether.onrender.com'
+                        : 'http://localhost:3000',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
+        },
+    };
 });
