d023395d4d810b198a0e47383ac41b50b7572b75
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-07-30 20:45:11 +0530

    fix: set input correctly after sanitization

--- client/src/components/input-panel/InputPanel.jsx
+++ client/src/components/input-panel/InputPanel.jsx
@@ -1,12 +1,20 @@
 import { memo } from 'react';
+import DOMPurify from 'dompurify';
 
 /**
  * InputPanel component for providing stdin for code execution.
- * @param {string} input - The current input vaue.
- * @param {Function} onInputChange - Callback for input changes.
+ * @param {Object} props - The component props.
+ * @param {React.ComponentState<string>} props.input - The current input state value.
+ * @param {React.SetStateAction<Function>} props.setInput - State setter for input.
  * @returns {JSX.Element} The input panel.
  */
-function InputPanel({ input, onInputChange }) {
+function InputPanel({ input, setInput }) {
+    function handleInputChange(event) {
+        const sanitizedInput = DOMPurify.sanitize(event.target.value);
+
+        setInput(sanitizedInput);
+    }
+
     return (
         <div
             className="flex h-full max-h-full flex-col gap-1 p-4 text-gray-800 dark:bg-[#222233] dark:text-gray-200"
@@ -19,7 +27,7 @@ function InputPanel({ input, onInputChange }) {
             <textarea
                 id="stdin"
                 value={input}
-                onChange={(e) => onInputChange(e.target.value)}
+                onChange={handleInputChange}
                 className="h-24 min-h-11 w-full rounded border border-gray-500 bg-gray-100 p-2 transition-shadow duration-300 focus:ring-2 focus:ring-blue-500 focus:outline-none dark:bg-[#2b2b44] dark:text-white"
                 placeholder="Enter input for your code (stdin)..."
                 aria-label="Enter input for code execution"
