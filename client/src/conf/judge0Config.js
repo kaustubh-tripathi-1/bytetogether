caccf13b5dc3c63573a275a2d3e482e24c2e0099
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-08-04 01:28:24 +0530

    fix: disable process and thread time/memory limits and increase other limits slightly

--- client/src/conf/judge0Config.js
+++ client/src/conf/judge0Config.js
@@ -3,13 +3,13 @@
  */
 export const judge0Limits = {
     cpu_time_limit: '3.0', // 3s
-    cpu_extra_time: '0.5', // Safety buffer
-    wall_time_limit: '5.0', // Total runtime
-    memory_limit: 131072, // 128 MB
+    cpu_extra_time: '1.0', // Safety buffer
+    wall_time_limit: '6.0', // Total runtime
+    memory_limit: 262144, // 256 MB
     stack_limit: 65536, // 64 MB
-    max_processes_and_or_threads: 32,
-    enable_per_process_and_thread_time_limit: true,
-    enable_per_process_and_thread_memory_limit: true,
+    max_processes_and_or_threads: 64,
+    enable_per_process_and_thread_time_limit: false,
+    enable_per_process_and_thread_memory_limit: false,
     max_file_size: 4096, // 4 KB
     enable_network: false, // False for safety
 };
