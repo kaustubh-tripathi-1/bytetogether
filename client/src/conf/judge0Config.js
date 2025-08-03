b6b429738379c17e049e934fcd562f8dc52760fb
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-08-03 17:06:47 +0530

    feat: add judge0 verdicts map/config

--- client/src/conf/judge0Config.js
+++ client/src/conf/judge0Config.js
@@ -5,11 +5,22 @@ export const judge0Limits = {
     cpu_time_limit: '3.0', // 3s
     cpu_extra_time: '0.5', // Safety buffer
     wall_time_limit: '5.0', // Total runtime
-    memory_limit: 128000, // 128 MB
-    stack_limit: 64000, // 64 MB
+    memory_limit: 131072, // 128 MB
+    stack_limit: 65536, // 64 MB
     max_processes_and_or_threads: 32,
     enable_per_process_and_thread_time_limit: true,
     enable_per_process_and_thread_memory_limit: true,
     max_file_size: 4096, // 4 KB
-    enable_network: false, // Always keep false for safety
+    enable_network: false, // False for safety
+};
+
+/**
+ * Judge0 Verdicts
+ */
+export const judge0Verdicts = {
+    3: `✅ Successfully executed`,
+    5: `💥 Time Limit Exceeded`,
+    6: `❌ Compilation Error`,
+    7: `🛑 Runtime Error`,
+    13: `🚫 Memory Limit Exceeded`,
 };
