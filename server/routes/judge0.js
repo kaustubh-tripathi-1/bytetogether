f77e5bc155020b5dc65d0b94a45a45e5fe9a8826
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-08-03 17:07:41 +0530

    fix: import env vars when not in prod

--- server/routes/judge0.js
+++ server/routes/judge0.js
@@ -2,6 +2,11 @@ import { Buffer } from 'node:buffer';
 import express from 'express';
 import axios from 'axios';
 
+if (process.env.NODE_ENV !== 'production') {
+    const dotenv = await import('dotenv');
+    dotenv.config();
+}
+
 const router = express.Router();
 const JUDGE0_URL = process.env.JUDGE0_URL || 'https://judge0-ce.p.rapidapi.com';
 const JUDGE0_API_KEY = process.env.JUDGE0_API_KEY;
@@ -13,24 +18,16 @@ const JUDGE0_API_KEY = process.env.JUDGE0_API_KEY;
  */
 router.post('/submissions', async (req, res) => {
     try {
-        const body = req.body;
-        if (body.source_code) {
-            body.source_code = Buffer.from(body.source_code, 'base64').toString(
-                'base64'
-            );
-        }
-        if (body.stdin) {
-            body.stdin = Buffer.from(body.stdin, 'base64').toString('base64');
-        }
+        console.log(req.body.source_code, typeof req.body.source_code);
 
         const response = await axios.post(
-            `${JUDGE0_URL}/submissions${req.query.base64_encoded ? '?base64_encoded=true' : ''}`,
+            `${JUDGE0_URL}/submissions${req.query.base64_encoded ? '?base64_encoded=true' : ''}&wait=false&fields=*`,
             req.body,
             {
                 headers: {
                     'content-type': 'application/json',
                     'x-rapidapi-key': JUDGE0_API_KEY,
-                    'X-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+                    'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
                 },
             }
         );
@@ -52,7 +49,7 @@ router.post('/submissions', async (req, res) => {
                 headers: {
                     'content-type': 'application/json',
                     'x-rapidapi-key': JUDGE0_API_KEY,
-                    'X-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+                    'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
                 },
                 body: JSON.stringify(req.body),
             }
