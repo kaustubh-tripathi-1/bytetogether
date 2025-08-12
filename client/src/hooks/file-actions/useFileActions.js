3f366defc2a2d3bec40e58adc74038f381b2d754
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-08-13 04:09:51 +0530

    feat: add setFilesForWebMode for adding html, css and js files for web mode

--- client/src/hooks/file-actions/useFileActions.js
+++ client/src/hooks/file-actions/useFileActions.js
@@ -1,22 +1,26 @@
 import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { ID } from 'appwrite';
 
 import { setSelectedFile } from '../../store/slices/editorSlice';
 import { getOrCreateYDoc } from '../../lib/yjs';
-import { getLanguageFromFileName } from '../../utils/getLanguageFromFileName';
 import {
     saveAllFilesForNewProject,
+    setFiles,
     updateAllFilesForExistingProject,
 } from '../../store/slices/filesSlice';
 import { addNotification } from '../../store/slices/uiSlice';
+import { getDefaultCodeForLanguage } from '../../utils/getDefaultCodeForLanguage';
+import { setCss, setHtml, setJs } from '../../store/slices/previewSlice';
 
 export function useFileActions({
     files,
-    username,
+    profile,
     isAdmin,
     selectedFile,
     isYjsConnected,
-    isNewProject,
+    // isNewProject,
+    activeProject,
     setIsFileExplorerOpen,
 }) {
     const dispatch = useDispatch();
@@ -25,13 +29,7 @@ export function useFileActions({
     useEffect(() => {
         if (files.length > 0 && !selectedFile) {
             const initialFile = files[0];
-            dispatch(
-                setSelectedFile({
-                    $id: initialFile.$id,
-                    fileName: initialFile.fileName,
-                    codeContent: initialFile.codeContent,
-                })
-            );
+            dispatch(setSelectedFile(initialFile));
         }
     }, [dispatch, files, selectedFile]);
 
@@ -48,11 +46,13 @@ export function useFileActions({
     const handleSaveAllFiles = useCallback(async () => {
         try {
             const filesToSave = files.map((file) => {
-                const { yText } = getOrCreateYDoc(file.$id, username, isAdmin);
+                const { yText } = getOrCreateYDoc(
+                    file.$id,
+                    profile?.username,
+                    isAdmin
+                );
                 return {
-                    $id: file.$id,
-                    name: file.fileName,
-                    language: getLanguageFromFileName(file.fileName),
+                    ...file,
                     codeContent:
                         file.$id === selectedFile?.$id && isYjsConnected
                             ? yText.toString()
@@ -60,10 +60,14 @@ export function useFileActions({
                 };
             });
 
-            if (isNewProject) {
+            await dispatch(
+                updateAllFilesForExistingProject(filesToSave)
+            ).unwrap();
+
+            /* if (isNewProject) {
                 await dispatch(
                     saveAllFilesForNewProject({
-                        projectName: 'default',
+                        projectId: activeProject?.$id || 'defaultProject',
                         files: filesToSave.filter((file) => !file.$id), // Only new files
                     })
                 ).unwrap();
@@ -73,7 +77,7 @@ export function useFileActions({
                         filesToSave.filter((file) => file.$id) // Only existing files
                     )
                 ).unwrap();
-            }
+            } */
 
             dispatch(
                 addNotification({
@@ -95,14 +99,72 @@ export function useFileActions({
         dispatch,
         files,
         isAdmin,
-        isNewProject,
+        // isNewProject,
         isYjsConnected,
         selectedFile,
-        username,
+        profile,
+        // activeProject,
     ]);
+
+    const setFilesForWebMode = useCallback(async () => {
+        try {
+            const defaultHtmlCode = getDefaultCodeForLanguage('html');
+            const defaultCssCode = getDefaultCodeForLanguage('css');
+            const defaultJsCode = getDefaultCodeForLanguage('javascript');
+
+            const webModeFiles = [
+                {
+                    $id: ID.unique(),
+                    projectId: activeProject?.$id || 'defaultProject',
+                    fileName: 'index.html',
+                    language: 'html',
+                    codeContent: defaultHtmlCode,
+                    ownerId: profile?.$id,
+                },
+                {
+                    $id: ID.unique(),
+                    projectId: activeProject?.$id || 'defaultProject',
+                    fileName: 'style.css',
+                    language: 'css',
+                    codeContent: defaultCssCode,
+                    ownerId: profile?.$id,
+                },
+                {
+                    $id: ID.unique(),
+                    projectId: activeProject?.$id || 'defaultProject',
+                    fileName: 'script.js',
+                    language: 'javascript',
+                    codeContent: defaultJsCode,
+                    ownerId: profile?.$id,
+                },
+            ];
+
+            dispatch(setFiles(webModeFiles));
+            dispatch(setHtml(defaultHtmlCode));
+            dispatch(setCss(defaultCssCode));
+            dispatch(setJs(defaultJsCode));
+
+            await dispatch(
+                saveAllFilesForNewProject({
+                    projectId: activeProject?.$id || 'defaultProject',
+                    files: webModeFiles,
+                })
+            ).unwrap();
+        } catch (error) {
+            console.error(error);
+
+            dispatch(
+                addNotification({
+                    message: error,
+                    type: 'error',
+                })
+            );
+        }
+    }, [activeProject, dispatch, profile]);
 
     return {
         handleSaveAllFiles,
         toggleFileExplorer,
+        setFilesForWebMode,
     };
 }
