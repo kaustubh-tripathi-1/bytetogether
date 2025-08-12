28906cf95e2ec4eeb26f3344ef8bf099eb78bdb0
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-08-13 01:44:15 +0530

    feat: set areFilesSaved as soon as user changes codeContent

--- client/src/components/code-editor/CodeEditor.jsx
+++ client/src/components/code-editor/CodeEditor.jsx
@@ -15,6 +15,7 @@ import { Spinner } from '../componentsIndex';
 import { addNotification } from '../../store/slices/uiSlice';
 import { useDebounce } from '../../hooks/useDebounce';
 import { setCss, setHtml, setJs } from '../../store/slices/previewSlice';
+import { setAreFilesSaved } from '../../store/slices/filesSlice';
 
 import nightOwlTheme from './themes/night-owl.json';
 import vsLight from './themes/custom-light.json';
@@ -36,6 +37,7 @@ function CodeEditor({ ref: editorRef, yjsResources, isYjsConnected }) {
     );
     const { theme } = useSelector((state) => state.ui);
     const { executionMode } = useSelector((state) => state.execution);
+    const { areFilesSaved } = useSelector((state) => state.files);
     const { yDoc, yText, awareness } = yjsResources;
 
     const bindingRef = useRef(null); // To store the MonacoBinding instance for cleanup
@@ -59,6 +61,9 @@ function CodeEditor({ ref: editorRef, yjsResources, isYjsConnected }) {
 
             dispatch(setCodeContent(value));
             dispatch(setSelectedFileContent(value));
+            if (areFilesSaved) {
+                dispatch(setAreFilesSaved(false));
+            }
 
             if (executionMode === 'preview') {
                 switch (language) {
@@ -77,7 +82,7 @@ function CodeEditor({ ref: editorRef, yjsResources, isYjsConnected }) {
                 }
             }
         },
-        [dispatch, executionMode, language, yText]
+        [areFilesSaved, dispatch, executionMode, language, yText]
     );
 
     const debouncedHandleContentChange = useDebounce(handleContentChange, 400);
